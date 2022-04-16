9baa84b04da6b7e6debf6762b1705b6fa146527e
CentChuan <609833577@qq.com>
2022-04-16 17:02:08 +0800

    删除

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## 运行步骤
 
-1. 运行 yarn
-2. 运行 yarn dev
+1. 运行 npm
+2. 运行 npm run dev
 3. 打开 http://127.0.0.1:3000
 
 ## 官网打包步骤
-1. 运行 yarn
-2. 运行 yarn build
+1. 运行 npm
+2. 运行 npm run build
 3. 得到的 dist 目录就是官网源代码
 4. 编辑并运行 sh deploy.sh 就可以上传到 Github 或码云等支持 Pages 功能的平台
 
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 rm -rf dist &&
-yarn build &&
+npm run build &&
 cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@gitee.com:frankfang/gulu-ui-website-1.git &&
+git remote add origin git@github.com:wuchuan123/vue3-wheels-dist.git &&
 git push -f -u origin master &&
 cd -
-echo https://frankfang.gitee.io/gulu-ui-website-1/
