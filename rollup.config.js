a166ee326d22e36ac2561fec17f95269d49d9261
CentChuan <609833577@qq.com>
2022-04-16 17:55:29 +0800

    优化

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import esbuild from 'rollup-plugin-esbuild'
-import vue from 'rollup-plugin-vue'
-import scss from 'rollup-plugin-scss'
-import dartSass from 'sass';
-import { terser } from "rollup-plugin-terser"
+import esbuild from 'rollup-plugin-esbuild' // ts变
+import vue from 'rollup-plugin-vue' // vue 变成js
+import scss from 'rollup-plugin-scss' // scss 变成js
+import dartSass from 'sass';  // 支持scss插件
+import { terser } from "rollup-plugin-terser" // 丑化js
 
 export default {
   input: 'src/lib/index.ts',
--- src/markdown/install.md
+++ src/markdown/install.md
@@ -3,13 +3,13 @@
 打开终端运行下列命令：
 
 ```
-npm install gulu-ui-1
+npm install vue3-wheels-dist
 ```
 
 或
 
 ```
-yarn add gulu-ui-1
+yarn add vue3-wheels-dist
 ```
 
 下一节：[开始使用](#/doc/get-started)
